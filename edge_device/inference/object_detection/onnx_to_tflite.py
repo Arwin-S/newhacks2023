77b1a982e669c669cb29f6de1000ac83dce7d348
Arwin Sondhi <arwin.sondhi@mail.utoronto.ca>
2023-11-05 06:35:15 -0500

    cleaned up dir

--- edge_device/inference/object_detection/onnx_to_tflite.py
+++
@@ -1,8 +0,0 @@
-from ultralytics import YOLO
-
-# Load a model
-# model = YOLO('best.onnx')  # load an official model
-model = YOLO('retail.pt')  # load an official model
-
-
-model.export(format='tflite')
--- edge_device/inference/object_detection/pt_to_tflite.py
+++
@@ -1,8 +0,0 @@
-from ultralytics import YOLO
-
-# Load a model
-model = YOLO('yolov3-tinyu.pt')  # load an official model
-# model = YOLO('retail.pt')  # load an official model
-
-
-model.export(format='tflite')
